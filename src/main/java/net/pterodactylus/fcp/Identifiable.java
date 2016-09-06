a30d521e1add957b2ddfee455bc6e061efbf44b7
David ‘Bombe’ Roden <bombe@freenetproject.org>
2016-09-06 20:32:29 +0200

    Update license to GPLv3, fix header comments

--- src/main/java/net/pterodactylus/fcp/Identifiable.java
+++ src/main/java/net/pterodactylus/fcp/Identifiable.java
@@ -1,5 +1,5 @@
 /*
- * jFCPlib - Identifiable.java - Copyright © 2010 David Roden
+ * jFCPlib - Identifiable.java - Copyright © 2010–2016 David Roden
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
