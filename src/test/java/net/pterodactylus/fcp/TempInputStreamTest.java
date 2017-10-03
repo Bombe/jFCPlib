43c9b2ad0e27b4b0bd90c2189f7791f8d8ae5a1a
Meteorite <alexey.meteorite@gmail.com>
2017-10-03 22:24:08 +0300

    switch to LF and tabs in TestInputStreamTest.java, remove Idea's stuff from project's gitignore

--- src/test/java/net/pterodactylus/fcp/TempInputStreamTest.java
+++ src/test/java/net/pterodactylus/fcp/TempInputStreamTest.java
@@ -14,41 +14,41 @@ import static org.junit.Assert.assertArrayEquals;
 
 public class TempInputStreamTest {
 
-    private byte[] prepareArrayOfNBytes(int n) {
-        byte[] data = new byte[n];
-        for (int i = 0;  i < n;  ++i) {
-            data[i] = (byte)i;
-        }
-        return data;
-    }
+	private byte[] prepareArrayOfNBytes(int n) {
+		byte[] data = new byte[n];
+		for (int i = 0;  i < n;  ++i) {
+			data[i] = (byte)i;
+		}
+		return data;
+	}
 
-    private void checkTempInputStreamStoresPartOfAnotherStream(int length, int maxMemoryLength) throws IOException {
-        byte[] originalData = prepareArrayOfNBytes(length + 1);
-        InputStream anotherStream = new ByteArrayInputStream(originalData);
-        FcpUtils.TempInputStream cut = new FcpUtils.TempInputStream(anotherStream, length, maxMemoryLength);
+	private void checkTempInputStreamStoresPartOfAnotherStream(int length, int maxMemoryLength) throws IOException {
+		byte[] originalData = prepareArrayOfNBytes(length + 1);
+		InputStream anotherStream = new ByteArrayInputStream(originalData);
+		FcpUtils.TempInputStream cut = new FcpUtils.TempInputStream(anotherStream, length, maxMemoryLength);
 
-        // check length bytes are read from anotherStream and are accessible from cut
-        byte[] buffer = new byte[length];
-        int n = cut.read(buffer);
-        assertThat(n, is(length));
-        assertArrayEquals(Arrays.copyOf(originalData, length), buffer);
-        assertThat(cut.read(), is(-1)); // check end of cut stream
+		// check length bytes are read from anotherStream and are accessible from cut
+		byte[] buffer = new byte[length];
+		int n = cut.read(buffer);
+		assertThat(n, is(length));
+		assertArrayEquals(Arrays.copyOf(originalData, length), buffer);
+		assertThat(cut.read(), is(-1)); // check end of cut stream
 
-        // check the rest of data in anotherStream is still there
-        n = anotherStream.read(buffer);
-        assertThat(n, is(1));
-        assertThat(buffer[0], is(originalData[originalData.length - 1]));
-        assertThat(anotherStream.read(), is(-1)); // check end of another stream
-    }
+		// check the rest of data in anotherStream is still there
+		n = anotherStream.read(buffer);
+		assertThat(n, is(1));
+		assertThat(buffer[0], is(originalData[originalData.length - 1]));
+		assertThat(anotherStream.read(), is(-1)); // check end of another stream
+	}
 
-    @Test
-    public void tempInputStreamShouldCorrectlyStorePartOfAnotherStreamInMemory() throws IOException  {
-        checkTempInputStreamStoresPartOfAnotherStream(1, 1);
-    }
+	@Test
+	public void tempInputStreamShouldCorrectlyStorePartOfAnotherStreamInMemory() throws IOException  {
+		checkTempInputStreamStoresPartOfAnotherStream(1, 1);
+	}
 
-    @Test
-    public void tempInputStreamShouldCorrectlyStorePartOfAnotherStreamInFile() throws IOException  {
-        checkTempInputStreamStoresPartOfAnotherStream(2, 1);
-    }
+	@Test
+	public void tempInputStreamShouldCorrectlyStorePartOfAnotherStreamInFile() throws IOException  {
+		checkTempInputStreamStoresPartOfAnotherStream(2, 1);
+	}
 
 }
