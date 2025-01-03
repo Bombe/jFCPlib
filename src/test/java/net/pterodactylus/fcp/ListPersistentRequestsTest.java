c11aeb1187ebdcc505ede02680c1ffa8e762b974
David ‘Bombe’ Roden <bombe@freenetproject.org>
2025-01-03 12:50:55 +0100

    ✅ Add test for ListPersistentRequests

---
+++ src/test/java/net/pterodactylus/fcp/ListPersistentRequestsTest.java
@@ -0,0 +1,23 @@
+package net.pterodactylus.fcp;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
+
+public class ListPersistentRequestsTest {
+
+	@Test
+	public void listPersistentRequestsHasCorrectName() {
+		ListPersistentRequests listPersistentRequests = new ListPersistentRequests();
+		assertThat(listPersistentRequests.getName(), equalTo("ListPersistentRequests"));
+	}
+
+	@Test
+	public void newListPersistentRequestsHasNoIdentifier() {
+		ListPersistentRequests listPersistentRequests = new ListPersistentRequests();
+		assertThat(listPersistentRequests.getField("Identifier"), nullValue());
+	}
+
+}
